c51fda9fadbe8c5e455a7477d261fa89b3fc3c13
ZhangEthan <228477911@qq.com>
2016-03-10 18:16:09 +0800

    twosum

--- src/main/java/edu/cqupt/ethan/leetcode/twosum/TwoSum.java
+++ src/main/java/edu/cqupt/ethan/leetcode/twosum/TwoSum.java
@@ -6,4 +6,10 @@ package edu.cqupt.ethan.leetcode.twosum;
  * @date 16/3/10
  */
 public class TwoSum {
+    public static void main(String[] args){
+        int[] a= {2,7,11,15};
+        Solution solution = new Solution();
+        System.out.println(solution.twoSum(a, 9));
+    }
 }
+
