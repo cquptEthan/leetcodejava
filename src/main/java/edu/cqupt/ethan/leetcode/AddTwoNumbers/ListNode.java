63ec7fbcdd76dff69682db100191e677b7b6c535
ZhangEthan <228477911@qq.com>
2016-03-11 09:28:17 +0800

    Add Two Numbers

---
+++ src/main/java/edu/cqupt/ethan/leetcode/AddTwoNumbers/ListNode.java
@@ -0,0 +1,12 @@
+package edu.cqupt.ethan.leetcode.AddTwoNumbers;
+
+/**
+ * @author Ethan
+ * @comment
+ * @date 16/3/10
+ */
+public class ListNode {
+             int val;
+             ListNode next;
+             ListNode(int x) { val = x; }
+}
