58d4875533a148179f6c782234bd5089af52c7bd
cquptethan <cquptethan@gmail.com>
2018-11-26 11:10:39 +0800

    98

---
+++ 98.validate-binary-search-tree.java
@@ -0,0 +1,82 @@
+import java.util.LinkedList;
+
+/*
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (24.61%)
+ * Total Accepted:    312.5K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * 
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * Output: false
+ * Explanation: The input is: [5,1,4,null,null,3,6]. The root node's
+ * value
+ * is 5 but its right child's value is 4.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // 中序遍历 然后 判断数组有序
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            if (!stack.isEmpty()) {
+                cur = stack.pop();
+                list.add(cur.val);
+                cur = cur.right;
+            }
+        }
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) >= list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
